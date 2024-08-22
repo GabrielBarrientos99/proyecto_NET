8327c72690358f8358473a9a6c109647187115e7
IsabelGarciaXX <isabelcielogarciahuapaya@gmail.com>
2024-08-22 17:15:22 -0500

    Cambios nuevos

---
+++ Models/GestionarVehiculoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PROYECTO_FLK.Models
+{
+    public class GestionarVehiculoViewModel
+    {
+        public List<Vehiculo> Vehiculos { get; set; } = new List<Vehiculo>();
+        public List<TipoDeVehiculo> TipoDeVehiculos { get; set; } = new List<TipoDeVehiculo>();
+
+     
+    }
+}
